44771ddd8731efc52793b53b63ae94e9d4e9f069
Zhiyuan <zy_pan@zju.edu.cn>
2022-04-22 02:24:41 +0000

    Implement memset

---
+++ include/string.h
@@ -0,0 +1,6 @@
+#ifndef _STRING_H_
+#define _STRING_H_
+
+void* memset(void *, int, unsigned long);
+
+#endif /* _STRING_H_ */
---
+++ lib/string.c
@@ -0,0 +1,6 @@
+void *memset(void *dst, int c, unsigned long n) {
+    char *cdst = (char *)dst;
+    for (unsigned long i = 0; i < n; ++i)
+        cdst[i] = c;
+    return dst;
+}
